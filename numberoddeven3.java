0a3f7cc6024c4ef4a6efea998e2f111cff448a01
ekta <aektapatel0102@gmail.com>
2022-07-29 00:18:52 +0100

    my second learning java prg

---
+++ numberoddeven3.java
@@ -0,0 +1,20 @@
+package Ifstatement3;
+
+public class numberoddeven3 {
+    public static void main(String[] args) {
+        int x =13;
+        if(x % 2 ==1)
+        {
+            System.out.println(" number is odd");
+
+
+        }
+        int y =8;
+        if(y % 2 ==0)
+        {
+            System.out.println("number is even");
+
+        }
+    }
+}
+
